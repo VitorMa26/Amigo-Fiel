6e17f76f34b9621074510a4c61946358fc6c4baa
Vitor Barros <vitor.m.barros7@aluno.senai.br>
2026-04-21 17:48:03 -0300

    refactor: enhance pet list component; implement filtering and update template bindings

--- src/app/pages/pet-list/pet-list.ts
+++ src/app/pages/pet-list/pet-list.ts
@@ -1,11 +1,12 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Pet } from '../../services/pet';
 import { PetModel } from '../../models/pets';
 import { PetCard } from '../../components/pet-card/pet-card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pet-list',
-  imports: [PetCard],
+  imports: [PetCard, FormsModule],
   templateUrl: './pet-list.html',
   styleUrl: './pet-list.css',
 })
@@ -13,13 +14,27 @@ export class PetList implements OnInit {
   private petService = inject(Pet);
   pets = signal<PetModel[]>([]);
   loading = signal(true);
+  nameFilter = '';
+  specieFilter = 'todos';
+  filteredList = signal<PetModel[]>([]);
 
   ngOnInit(): void {
     this.petService.getAll().subscribe({
       next: (res) => {
         this.loading.set(false);
         this.pets.set(res);
+        this.filteredList.set(res);
       },
     });
+  }
+
+  filterResults() {
+    const newList = this.pets().filter((item) =>
+      item.name.toLowerCase().includes(this.nameFilter.toLowerCase()),
+    );
+    this.filteredList.set(newList);
+    if (this.nameFilter.length == 0) {
+      this.filteredList.set(this.pets());
+    }
   }
 }
